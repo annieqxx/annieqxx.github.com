2293e2fd4a112ae1df7c40b70dbc4cbfd96ff5fd
Xiaoxiao(Annie) Qin <annieqxx@scc2102-12.ucdavis.edu>
2020-02-20 11:51:07 -0800

    academic research

--- des157-wi20/index.js
+++ des157-wi20/index.js
@@ -9,7 +9,7 @@ function setup() {
   
 
   
-  function heartShape(x, y, z){
+  function heartShape(x, y){
   //  bezierVertex(x2, y2, x3, y3, x4, y4)
   //bezierVertex(x2, y2, z2, x3, y3, z3, x4, y4, z4)
   
@@ -17,17 +17,17 @@ function setup() {
   noStroke();
   fill(random(255), random(255), random(255), random(255));
   beginShape();
-  vertex((50+x)*z, (15+y)*z);
-  bezierVertex((50+x)*z, (-5+y)*z, (90+x)*z, (5+y)*z, (50+x)*z, (40+y)*z);
-  vertex((50+x)*z, (15+y)*z);
-  bezierVertex((50+x)*z, z*(-5+y), (10+x)*z, z*(5+y), z*(50+x), (40+y)*z);
+  vertex((50+x), (15+y));
+  bezierVertex((50+x), (-5+y), (90+x), (5+y), (50+x), (40+y));
+  vertex((50+x), (15+y));
+  bezierVertex((50+x), (-5+y), (10+x), (5+y), (50+x), (40+y));
   endShape();
   }
   
   
   function draw(){
   
-    var size = int(random(1,4));
-    heartShape(mouseX-50, mouseY-15, size);
+    // var size = int(random(1,3));
+    heartShape(mouseX-50, mouseY-15);
   
   }
